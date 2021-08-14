ea4e1f4cf81283fdf114e64e716946eb30baa57d
asagafonov <a.s.agafonov@yandex.ru>
2021-08-14 12:41:11 +0300

    add button and controllers consistent display on popup reopen

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -5,23 +5,31 @@ const pauseBtn = document.querySelector('.pause');
 const speed = document.querySelector('#rangemeter');
 const offset = document.querySelector('#offsetmeter');
 
-[playBtn, pauseBtn].forEach((el) => el.addEventListener('click', () => {
+const chooseBtnType = (response) => {
+  switch (response.btnStatus) {
+    case true:
+      playBtn.style.display = 'block';
+      pauseBtn.style.display = 'none';
+      break;
+    case false:
+      playBtn.style.display = 'none';
+      pauseBtn.style.display = 'block';
+      break;
+    default:
+      throw Error(`No such response type as ${response}`);
+  }
+};
+
+chrome.runtime.sendMessage({ button: 'check' }, (response) => {
+  chooseBtnType(response);
+  speed.value = response.frequency;
+  offset.value = response.offsetY;
+});
+
+[playBtn, pauseBtn].forEach((btn) => btn.addEventListener('click', () => {
   chrome.runtime.sendMessage({
     button: 'clicked',
     speed: speed.value,
     offset: offset.value,
-  }, (response) => {
-    switch (response) {
-      case true:
-        playBtn.style.display = 'block';
-        pauseBtn.style.display = 'none';
-        break;
-      case false:
-        playBtn.style.display = 'none';
-        pauseBtn.style.display = 'block';
-        break;
-      default:
-        throw Error(`No such response type as ${response}`);
-    }
-  });
+  }, (response) => chooseBtnType(response));
 }));
