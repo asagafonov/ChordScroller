ec3e10dfc0abcd57504ffb579551e59af89f2551
asagafonov <a.s.agafonov@yandex.ru>
2021-08-26 00:33:00 +0300

    move handlers to a separate file

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,88 +1,10 @@
 import './popup.css';
-import { stepValues } from '../utils/values';
+import { handleResponse, handleDisconnect } from '../utils/handlers';
 import elements from '../utils/elements';
 
 const {
-  playBtn,
-  pauseBtn,
-  frqMeter,
-  offsetMeter,
-  frqValueBox,
-  offsetValueBox,
-  frqDescriptionBox,
-  offsetDescriptionBox,
-  authorLink,
-  inputContainer,
+  playBtn, pauseBtn, frqMeter, offsetMeter, authorLink,
 } = elements;
-
-const chooseBtnType = (response) => {
-  switch (response.btnStatus) {
-    case true:
-      playBtn.style.display = 'block';
-      pauseBtn.style.display = 'none';
-      authorLink.style.color = 'rgb(129, 212, 84)';
-      break;
-    case false:
-      playBtn.style.display = 'none';
-      pauseBtn.style.display = 'block';
-      authorLink.style.color = 'rgb(237, 35, 13)';
-      break;
-    default:
-      throw Error(`No such response type as ${response}`);
-  }
-};
-
-const handleResponse = (response) => {
-  const { action, frequency, offsetY } = response;
-
-  switch (action) {
-    case 'checked':
-      chooseBtnType(response);
-      frqMeter.value = frequency;
-      frqValueBox.textContent = frqMeter.value;
-      frqValueBox.style.left = `${stepValues.value[frqMeter.value]}%`;
-      frqDescriptionBox.style.left = `${stepValues.description.speed[frqMeter.value]}%`;
-      frqDescriptionBox.textContent = 'speed';
-      offsetMeter.value = offsetY;
-      offsetValueBox.textContent = offsetMeter.value;
-      offsetValueBox.style.left = `${stepValues.value[offsetMeter.value]}%`;
-      offsetDescriptionBox.style.left = `${stepValues.description.offset[offsetMeter.value]}%`;
-      offsetDescriptionBox.textContent = 'offset';
-      break;
-    case 'clicked':
-      chooseBtnType(response);
-      break;
-    case 'input-changed':
-      frqValueBox.textContent = frqMeter.value;
-      frqValueBox.style.left = `${stepValues.value[frqMeter.value]}%`;
-      frqDescriptionBox.style.left = `${stepValues.description.speed[frqMeter.value]}%`;
-      offsetValueBox.textContent = offsetMeter.value;
-      offsetValueBox.style.left = `${stepValues.value[offsetMeter.value]}%`;
-      offsetDescriptionBox.style.left = `${stepValues.description.offset[offsetMeter.value]}%`;
-      break;
-    default:
-      throw Error(`No such action as ${response.action}`);
-  }
-};
-
-const handleDisconnect = () => {
-  const errorContainer = document.createElement('div');
-  const errorMessageOne = document.createElement('div');
-  const errorMessageTwo = document.createElement('div');
-  const br = document.createElement('br');
-
-  errorContainer.setAttribute('class', 'error');
-  errorMessageOne.textContent = 'ChordScroller couldn\'t establish connection.';
-  errorMessageTwo.textContent = 'Refreshing the page may fix this problem.';
-  errorMessageTwo.setAttribute('class', 'error error-accent');
-
-  inputContainer.innerHTML = '';
-
-  errorContainer.append(errorMessageOne);
-  errorContainer.append(br);
-  errorContainer.append(errorMessageTwo);
-  inputContainer.append(errorContainer);
-};
 
 window.addEventListener('DOMContentLoaded', () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
